cd022e104a90b63b40d9ec439710b0b5e62ce733
rania jaberi <raniajaberi31@gmail.com>
2022-09-10 19:20:32 +0200

    create main fragment

---
+++ app/src/main/java/com/example/wallpaperapplication/Fragments/FavoriteFragment.java
@@ -0,0 +1,29 @@
+package com.example.wallpaperapplication.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wallpaperapplication.R;
+
+
+public class FavoriteFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_favorite, container , false);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
---
+++ app/src/main/java/com/example/wallpaperapplication/Utils/Functions.java
@@ -0,0 +1,15 @@
+package com.example.wallpaperapplication.Utils;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.wallpaperapplication.R;
+
+public class Functions {
+   // public static void changeMainFragment( FragmentActivity fragmentActivity, Fragment fragment){
+      //  fragmentActivity.getSupportFragmentManager()
+       //         .beginTransaction()
+        //        .replace(R.id.main_container, fragment)
+       //         .commit();
+    //}
+}
