951dc1a275a1f578074275cbe513471921fb0555
rania jaberi <raniajaberi31@gmail.com>
2022-09-15 23:33:26 +0200

    Create PhotoAdapter

--- app/src/main/java/com/example/wallpaperapplication/Utils/WallPaperAppGlideModule.java
+++ app/src/main/java/com/example/wallpaperapplication/Utils/WallPaperAppGlideModule.java
@@ -1,7 +1,10 @@
 package com.example.wallpaperapplication.Utils;
 
+import android.content.Context;
+
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 @GlideModule
 public class WallPaperAppGlideModule extends AppGlideModule {
+
 }
